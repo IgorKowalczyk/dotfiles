fd0bef77f9c92f5afd2985cba7bf022a2fec6074
Igor Kowalczyk <majonezexe@protonmail.com>
2024-01-06 18:01:00 +0100

    Update pre-install.sh and zshrc

--- pre-install.sh
+++ pre-install.sh
@@ -7,7 +7,7 @@ RESET=$(printf '\033[0m')
 # Change default shell
 if [ ! $SHELL = "/usr/bin/zsh" ]; then
   echo "${BLUE}Changing default shell to zsh${RESET}"
-  chsh -s /bin/zsh
+  sudo chsh -s /bin/zsh
 else
   echo "${GREEN}Already using ZSH${RESET}"
 fi
@@ -16,16 +16,20 @@ fi
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
   echo "${BLUE}Installing oh-my-zsh${RESET}"
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
   echo "${GREEN}oh-my-zsh already installed${RESET}"
 fi
+
+# Install atuin
+bash -c "$(curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh)"
+atuin login
 
 # Check if nvm is installed
 NVM="$HOME/.nvm"
 if [ ! -d "$NVM" ]; then
    echo "${BLUE}Installing nvm${RESET}"
-   /bin/sh -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash)"
+   bash -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh)"
  else
   echo "${GREEN}NVM already installed${RESET}"
 fi
